0428659457d1b02a83434c0d6cf057620846b003
SooAh0208 <santacoyaki11@gmail.com>
2026-01-02 09:51:55 +0900

    NotFound

---
+++ src/pages/Page2.jsx
@@ -0,0 +1,3 @@
+const Page2 = () => <h1>2페이지입니다.</h1>
+
+export default Page2
---
+++ src/router/index.jsx
@@ -0,0 +1,30 @@
+// import { createBrowserRouter } from "react-router";
+// import { RouterProvider } from "react-router/dom";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Page1 from "@pages/Page1.jsx"
+import Page2 from "@pages/Page2.jsx"
+import NotFound from "@pages/NotFound.jsx"
+
+const Router = () =>{
+// const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   Component:Page1,
+  // },
+  //  {
+  //   path: "/page2",
+  //   Component:Page2,
+  // }
+// ]);
+  return (
+   <BrowserRouter>
+   <Routes>
+    {/* <Route path ="/" element={<Page1 />} /> */}
+    <Route path ="/page2" element={<Page2 />} />
+    <Route path ="/" element={<NotFound />} />
+  
+   </Routes>
+   </BrowserRouter>
+  )
+}
+  export default Router
