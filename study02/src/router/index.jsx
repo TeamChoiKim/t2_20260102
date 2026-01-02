9f716715a01db7e44f8ab5d2c8945166771fe8c6
choiyc05 <choiyc05@naver.com>
2026-01-02 17:05:33 +0900

    260102_1700 upload

--- study02/src/router/index.jsx
+++ study02/src/router/index.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import NotFound from "@pages/NotFound.jsx"
+import HookState from "@pages/hookState"
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/state/*" element={<HookState />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
