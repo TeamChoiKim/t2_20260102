3418afc4670a5b5cf695e8030d7e05581209981c
choiyc05 <choiyc05@naver.com>
2026-01-05 09:43:38 +0900

    upload

--- study02/src/pages/hookstate/Create.jsx
+++ study02/src/pages/hookstate/Create.jsx
@@ -31,7 +31,7 @@ const Create = () => {
         <div className="d-flex">
           <div className="p-2 flex-fill">
             <div className="form-check">
-            <input type="radio" className="form-check-input" id="radio1" name="gender" value="1" onChange={onChange}/>남성
+            <input type="radio" className="form-check-input" id="radio1" name="gender" value="1" defaultChecked onChange={onChange}/>남성
             <label className="form-check-label" htmlFor="radio1"></label>
           </div>
           </div>
--- study02/src/pages/hookstate/Page1.jsx
+++ study02/src/pages/hookstate/Page1.jsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 const Page1 = () => {
     const [data, setData] = useState(1)
     let 변수 = 0
-    console.log(data, 변수)
+    // console.log(data, 변수)
 
     const event = () => {
         setData(data + 1)
